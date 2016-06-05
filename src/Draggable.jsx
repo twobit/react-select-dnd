66ee00daff526b28ba7f422386ebc26b9d94023a
Stephen Murphy <stephen@hypernaut.com>
2016-06-04 23:52:20 -0700

    minor tweaks

--- src/Draggable.jsx
+++ src/Draggable.jsx
@@ -1,97 +1,56 @@
 import React from 'react';
 import Select from 'react-select';
-import {DragDropContext, DragSource, DropTarget} from 'react-dnd';
+import {DragSource, DropTarget} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import classNames from 'classnames';
 
 const itemSource = {
-  beginDrag(props) {
-    return props.value;
+  beginDrag({id, value}) {
+    return {id, value};
   }
 };
 
 const itemTarget = {
-  hover(props, monitor) {
+  hover({id, value, onChangeLayout}, monitor) {
     const item = monitor.getItem();
-    if (item !== props.value) {
-      props.onChangeLayout(item, props.value);
+    if (item.id !== id) {
+      onChangeLayout(item.value, value);
     }
   }
 };
 
-function collectDrag(connect, monitor) {
-  return {
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging()
-  };
-}
+const collectDrag = (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+});
 
-function collectDrop(connect) {
-  return {
-    connectDropTarget: connect.dropTarget()
-  };
-}
+const collectDrop = connect => ({
+  connectDropTarget: connect.dropTarget()
+});
 
-var DraggableValue = function(Component) {
-  return (
-    DragSource("DraggableSelect", itemSource, collectDrag)(
-    DropTarget("DraggableSelect", itemTarget, collectDrop)(
-      React.createClass({
-        render() {
-          const {connectDragSource, connectDropTarget, isDragging, ...rest} = this.props;
-          return connectDragSource(connectDropTarget(
-            <span style={{opacity: isDragging ? 0.3 : null}}
-                  onMouseDown={e => e.stopPropagation()}>
-                    <Component {...rest} />
-            </span>
-          ));
-        }
-      })
-    ))
-  );
-};
-
-var DraggableSelect = function(Component, ValueComponent) {
-  return React.createClass({
-  	getDefaultProps () {
-  		return {
-			  valueKey: 'value'
-      };
-    },
-
-    onChangeLayout(src, dest) {
-      const {value, valueKey, onChange} = this.props,
-            values = value.map(d => d[valueKey]),
-            srcIndex = values.indexOf(src[valueKey]),
-            destIndex = values.indexOf(dest[valueKey]),
-            newValue = value.concat([]);
-
-      newValue.splice(srcIndex, 1);
-      newValue.splice(destIndex, 0, src);
-      onChange(newValue);
-    },
-
-  	render() {
-  		return (
-  			<Component {...this.props}
-  				multi={true}
-          onChangeLayout={this.onChangeLayout}
-          valueComponent={ValueComponent} />
-  		);
-  	}
-  });
-};
-
-export {DraggableSelect, DraggableValue};
+const DraggableValue = Component =>
+  DragSource("DraggableSelect", itemSource, collectDrag)(
+  DropTarget("DraggableSelect", itemTarget, collectDrop)(
+    class extends React.Component {
+      render() {
+        const {connectDragSource, connectDropTarget, isDragging, ...rest} = this.props;
+        return connectDragSource(connectDropTarget(
+          <span style={{opacity: isDragging ? 0.3 : null}}
+                onMouseDown={e => e.stopPropagation()}>
+                  <Component {...rest} />
+          </span>
+        ));
+      }
+    }
+  ));
 
 class SelectDND extends Select {
-	constructor(props) {
-		super(props);
-		this.renderValue = this._renderValue.bind(this);
-		this.onChangeLayout = this._onChangeLayout.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.valueComponent = DraggableValue(this.props.valueComponent);
+  }
 
-  _onChangeLayout(src, dest) {
+  onChangeLayout = (src, dest) => {
     const {value, valueKey, onChange} = this.props,
           values = value.map(d => d[valueKey]),
           srcIndex = values.indexOf(src[valueKey]),
@@ -101,11 +60,11 @@ class SelectDND extends Select {
     newValue.splice(srcIndex, 1);
     newValue.splice(destIndex, 0, src);
     onChange(newValue);
-  }
+  };
 
-	_renderValue(valueArray, isOpen) {
+	renderValue = (valueArray, isOpen) => {
 		let renderLabel = this.props.valueRenderer || this.getOptionLabel;
-		let ValueComponent = DraggableValue(this.props.valueComponent);
+		let ValueComponent = this.valueComponent;
 		if (!valueArray.length) {
 			return !this.state.inputValue ? <div className="Select-placeholder">{this.props.placeholder}</div> : null;
 		}
@@ -142,9 +101,9 @@ class SelectDND extends Select {
 				</ValueComponent>
 			);
 		}
-	}
+	};
 }
 
-const SelectDNDBackend = DragDropContext(HTML5Backend)(SelectDND);
+// const SelectDNDBackend = DragDropContext(HTML5Backend)(SelectDND);
 
-export {SelectDND, SelectDNDBackend};
+export {SelectDND};
